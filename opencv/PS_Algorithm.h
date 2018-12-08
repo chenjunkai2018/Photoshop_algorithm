fd4293f00aea5c4cc447191d55fa69b12a3968a1
chenjunkai2018 <shinian1987@126.com>
2018-12-08 10:11:09 +0800

    opencv

---
+++ opencv/PS_Algorithm.h
@@ -0,0 +1,19 @@
+
+// define head function
+#ifndef PS_ALGORITHM_H_INCLUDED
+#define PS_ALGORITHM_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include "cv.h"
+#include "highgui.h"
+#include "cxmat.hpp"
+#include "cxcore.hpp"
+#include "math.h"
+
+using namespace std;
+using namespace cv;
+
+void Show_Image(Mat&, const string &);
+
+#endif // PS_ALGORITHM_H_INCLUDED
